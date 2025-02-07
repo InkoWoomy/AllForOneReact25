ba1ab0f653b56a9c180a8054bef7a5f616b12696
Alexander <yoda4115@gmail.com>
2025-02-07 10:54:10 -0800

    Responsiveness done

--- src/App.jsx
+++ src/App.jsx
@@ -14,80 +14,77 @@ import { NavLink } from 'react-router-dom'
 
 
 function App() {
-
-
-  console.log("App is rendering")
   return (
-    <div>
-      <h1 className='text-7xl mb-4'>All For One</h1>
+    <div className='px-3'>
+      <h1 className='text-7xl'>All For One</h1>
       <div className='grid grid-cols-2 md:grid-cols-3 justify-items-center center'>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/say-hello'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Say Hello</h1>
+              <h1 className='text-3xl m-0'>Say Hello</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/add-two-numbers'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Add Two Numbers</h1>
+              <h1 className='text-3xl m-0'>Add Two Numbers</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/asking-questions'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Asking Questions</h1>
+              <h1 className='text-3xl m-0'>Asking Questions</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/greater-less'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Greater Than Or Less Than</h1>
+              <h1 className='text-3xl m-0'>Greater Than Or Less Than</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/mad-libs'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Mad Libs</h1>
+              <h1 className='text-3xl m-0'>Mad Libs</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/odd-even'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Odd Or Even</h1>
+              <h1 className='text-3xl m-0'>Odd Or Even</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/reverse-it-a'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Reverse It (Alphanumeric)</h1>
+              <h1 className='text-3xl m-0'>Reverse It (Alphanumeric)</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/reverse-it-n'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Reverse It (Numeric)</h1>
+              <h1 className='text-3xl m-0'>Reverse It (Numeric)</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2'>
+        <div className='m-5 w-full p-1'>
           <NavLink type='button' to={'/eight-ball'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Eight Ball</h1>
+              <h1 className='text-3xl m-0'>Eight Ball</h1>
             </Button>
           </NavLink>
         </div>
-        <div className='m-5 w-full p-2 col-start-2'>
+        <div className='m-5 w-full p-1 col-start-2'>
           <NavLink type='button' to={'/restaurant-picker'}>
             <Button className='w-full h-full flex items-center'>
-              <h1 className='text-3xl m-2'>Restaurant Picker</h1>
+              <h1 className='text-3xl m-0'>Restaurant Picker</h1>
             </Button>
           </NavLink>
         </div>
